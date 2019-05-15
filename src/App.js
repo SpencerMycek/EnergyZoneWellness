843cf9aa54d0023e3a9d1f09596f3b6bc2ac6771
Spencer Mycek <srm2686@rit.edu>
2019-05-14 22:38:19 -0400

    Change where navbar and footer are in jsx elements

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 
 function App() {
     return (
-        <div class="Main">
+        <div className="App">
             <div className="Navbar">Navbar</div>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React</h2>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
             <div className="Footer">Footer</div>
         </div>
     );
